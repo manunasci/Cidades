ce2f4c1cffb846faf3c77e82a49c7252a64b8fd6
manunasci <nascimentomanuella73@gmail.com>
2021-08-20 23:17:37 -0300

    Alteração no programa exibido

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cidade do litoral sul-SP");
+            Console.WriteLine("");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Peruíbe");
+            Console.WriteLine("Iguape");
+            Console.WriteLine("Ilha Comprida");
+            Console.WriteLine("Cananeia");
+        }
+    }
+}
